17533f4cfc71bf0f405f43f94ceb9834fa5a8c79
phongtintruong <trung.nt204857@sis.hust.edu.vn>
2023-07-30 01:57:40 +0700

    Reorganize lecture folder

--- practice/lecture/lecture_1/AddMod.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<math.h>
-using namespace std;
-int main(){
-    unsigned long long a, b;
-    cin>>a>>b;
-    cout<<((a%(int)(pow(10,9)+7))+(b%(int)(pow(10,9)+7)))%(int)(pow(10,9)+7);
-    return 0;
-}
